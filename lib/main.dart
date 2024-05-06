69bbd7531d2766cfc788b3d74f7127429c3421d1
421yet <34169103+421yet@users.noreply.github.com>
2024-05-06 03:30:08 -0500

    wtf man

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:kampai_webpage/amplifyconfiguration.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
@@ -13,8 +16,21 @@ void main() async {
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
 
+  WidgetsFlutterBinding.ensureInitialized();
+  await _configureAmplify();
+
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
   runApp(MyApp(settingsController: settingsController));
+}
+
+Future<void> _configureAmplify() async {
+  try {
+    await Amplify.configure(amplifyconfig);
+  } catch (e) {
+    if (kDebugMode) {
+      print('An error occured while configuring Amplify: $e');
+    }
+  }
 }
